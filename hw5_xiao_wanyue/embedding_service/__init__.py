94dcb7d0a54d9ec491ee55890dff5b0898abbf0d
xwanyue0221 <67293171+xwanyue0221@users.noreply.github.com>
2022-04-25 23:39:57 -0400

    Add files via upload

---
+++ hw5_xiao_wanyue/embedding_service/__init__.py
@@ -0,0 +1,4 @@
+# set the port number for an embedding server
+# we may want to be consistent about the port number for different servers to avoid conflicts.
+PORT_EMBEDDING_MAPPING = {8080: "sbert", 8081: "fasttext"}
+INV_PORT_EMBEDDING_MAPPING = {"sbert": 8080, "fasttext": 8081}
